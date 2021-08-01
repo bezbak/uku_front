32e721827ecfeccfa6184f09210592057acd4004
zetxc <a.ermek.kg@gmail.com>
2021-08-01 23:21:52 +0600

    - changed backend url

--- config.js
+++ config.js
@@ -2,15 +2,18 @@ const target = process.env.NEXT_PUBLIC_NODE_TARGET || 'development';
 
 const configs = {
   development: {
-    apiUrl: 'http://uku.kg/api/v1/',
+    // apiUrl: 'http://uku.kg/api/v1/',
+    apiUrl: "http://api.uku.kg/api/v1/",
     target,
   },
   staging: {
-    apiUrl: 'http://uku.kg/api/v1/',
+    // apiUrl: 'http://uku.kg/api/v1/',
+    apiUrl: "http://api.uku.kg/api/v1/",
     target,
   },
   production: {
-    apiUrl: 'http://uku.kg/api/v1/',
+    // apiUrl: 'http://uku.kg/api/v1/',
+    apiUrl: "http://api.uku.kg/api/v1/",
     target,
   },
 };
