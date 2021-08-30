08789c0afb922139c5706969eb7cc50e9985a93b
zetxc <a.ermek.kg@gmail.com>
2021-08-30 13:20:14 +0600

    - fixed cards

--- containers/mainfeed.js
+++ containers/mainfeed.js
@@ -1,25 +1,29 @@
 import React, {useEffect, useRef, useState} from 'react';
 import Card from "../components/Card";
 import uku from '/adapters/HTTP_Agent'
-import {useRecoilState} from "recoil";
 import {endpoints} from "../api/endpoints";
-import {toast} from "react-toastify";
+import styles from './styles.module.scss'
 
 const Mainfeed = () => {
   const [state, setState] = useState([])
   const ref = useRef(null)
 
   useEffect(() => {
-
+    // const token = window.localStorage
+    fetch(uku + endpoints.feed + `?page=${1}`, {}).then(r => {
+      r.json().then(data => setState(data.results))
+    })
   }, [])
 
   return (
     <div className={"container"}>
       <h2>Лента</h2>
-      <Card
-        width={"320px"}
-        cards={[]}
-      />
+      <div className={styles.feed}>
+        <Card
+          width={"320px"}
+          cards={state}
+        />
+      </div>
       <div ref={ref}/>
     </div>
   )
